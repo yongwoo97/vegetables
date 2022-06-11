c800975663b46e4ddfa147ea60a8134d8d62ca88
yongwoo97 <woo22661@naver.com>
2022-06-11 13:09:31 +0900

    url 수정

--- board/urls.py
+++ board/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from .views import ProductAddView, ProductView, LocationView, UnitView, ProductListView, ProductUpdateView
-from .views import ProductSearchView, ExcelMakerView, RevenueView, ProductConfirmView
+from .views import ProductSearchView, ExcelMakerView, RevenueView
 router = DefaultRouter()
 router.register(r'product', ProductView)
 router.register(r'location', LocationView)
